88773dd059d41344518e36383a6aee789fe8a467
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:30:47 -0300

    Add Consultoria class with initialization

---
+++ src/consultoria.py
@@ -0,0 +1,7 @@
+class Consultoria:
+    def __init__(self, id_consultoria, tema, horario, produtor, rede):
+        self.id_consultoria = id_consultoria
+        self.tema = tema
+        self.horario = horario
+        self.produtor = produtor
+        self.rede = rede
