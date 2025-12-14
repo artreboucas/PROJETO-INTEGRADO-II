d87f9192e2e725ca531f253bb75cb61c74d063ab
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:15:56 -0300

    Create Usuario class with user attributes

---
+++ src/usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, id_usuario, nome, email, senha, tipo_usuario):
+        self.id_usuario = id_usuario
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.tipo_usuario = tipo_usuario
