780dbf22c721cd9aaea0a12aff1f65af9a5411af
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:29:21 -0300

    Create Produtor class with initialization method

---
+++ src/produtor.py
@@ -0,0 +1,9 @@
+class Produtor:
+    def __init__(self, id_produtor, nome, tipo, local, email, usuario):
+        self.id_produtor = id_produtor
+        self.nome = nome
+        self.tipo = tipo
+        self.local = local
+        self.email = email
+        self.usuario = usuario  
+        self.consultorias = []
