e67387dea384c61e2f25b70a76c5300358032ed2
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:32:59 -0300

    Add Participacao class with initialization

---
+++ src/participacao.py
@@ -0,0 +1,7 @@
+class Participacao:
+    def __init__(self, id_participacao, produtor, capacitacao, presenca, status):
+        self.id_participacao = id_participacao
+        self.produtor = produtor
+        self.capacitacao = capacitacao
+        self.presenca = presenca
+        self.status = status
