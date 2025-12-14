4b7ae80e794d62e2881d1a42ebfa5f9b9d5e0d69
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:31:13 -0300

    Create Capacitacao class with initialization

---
+++ src/capacitacao.py
@@ -0,0 +1,8 @@
+class Capacitacao:
+    def __init__(self, id_capacitacao, titulo, descricao, data, rede):
+        self.id_capacitacao = id_capacitacao
+        self.titulo = titulo
+        self.descricao = descricao
+        self.data = data
+        self.rede = rede
+        self.arquivos = []
