8fef643a139bbdea212276df3d645682596479e5
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-12-13 22:30:27 -0300

    Create RedeDeApoio class with initialization

---
+++ src/rede_apoio.py
@@ -0,0 +1,9 @@
+class RedeDeApoio:
+    def __init__(self, id_rede, nome, area, foco, localizacao, usuario):
+        self.id_rede = id_rede
+        self.nome = nome
+        self.area = area
+        self.foco = foco
+        self.localizacao = localizacao
+        self.usuario = usuario
+        self.capacitacoes = []
